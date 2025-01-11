5a66d8f2d3b9039ae614be4cc7b4b73a20d160b1
Om Satapathy <om.satapthy@sonicjobs.net>
2025-01-11 08:55:57 +0530

    new webdriver code  merged

--- src/main/java/com/jan11/Webautomation.java
+++ src/main/java/com/jan11/Webautomation.java
@@ -18,51 +18,60 @@ public class Webautomation {
 
 //	@Test
 	public void verifyCountry() {
-		
+
 		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get("https://testautomationpractice.blogspot.com/");
-		
-		WebElement ele =driver.findElement(By.id("country"));
-		  
-		  Select sel= new Select(ele);
-		  sel.selectByVisibleText("France");
-		  
-		  
-		List<WebElement> allanimals =  driver.findElements(By.id("animals"));
-		for(WebElement animal : allanimals) {
+
+		WebElement ele = driver.findElement(By.id("country"));
+
+		Select sel = new Select(ele);
+		sel.selectByVisibleText("France");
+
+		List<WebElement> allanimals = driver.findElements(By.id("animals"));
+		for (WebElement animal : allanimals) {
 			System.out.println(animal.getText());
 		}
-		  
-		  
-		  JavascriptExecutor js = (JavascriptExecutor)driver;
-		  js.executeScript("window.scrollBy(0,1800)");
-		  
-		  
-		  SoftAssert softAssert = new SoftAssert();
-		  softAssert.assertAll();
-		  
-		  
-		  
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,1800)");
+
+		SoftAssert softAssert = new SoftAssert();
+		softAssert.assertAll();
+
 	}
-	
-	@Test
+
+//	@Test
 	public void softAssetExample() {
 		WebDriverManager.chromedriver().setup();
-	    ChromeDriver driver = new ChromeDriver();
-        SoftAssert softAssert = new SoftAssert();
-        driver.manage().window().maximize();
-        driver.get("https://www.geeksforgeeks.org/");
-        String actualRersult=driver.getTitle();
-    
-        String expectedResult="GeeksforGeeks ";
-        
-        softAssert.assertEquals(actualRersult,expectedResult,"Title is not Matching");
+		driver = new ChromeDriver();
+		SoftAssert softAssert = new SoftAssert();
+		driver.manage().window().maximize();
+		driver.get("https://www.geeksforgeeks.org/");
+		String actualRersult = driver.getTitle();
 
-        driver.findElement(By.linkText("Java")).click();
+		String expectedResult = "GeeksforGeeks ";
 
-        softAssert.assertAll();
-        driver.close();
+		softAssert.assertEquals(actualRersult, expectedResult, "Title is not Matching");
+
+		driver.findElement(By.linkText("Java")).click();
+
+		softAssert.assertAll();
+		driver.close();
+	}
+
+	@Test
+	public void framehandler() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.navigate().to("https://demoqa.com/frames");
+	//	WebElement ele = driver.findElement(By.id("frame1"));
+
+		driver.switchTo().frame(0);
+		//System.out.println();
+
 	}
 
 }
